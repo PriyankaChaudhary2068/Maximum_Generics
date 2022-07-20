8f09da17e0c9d7fdc781f762195ecb7ca5bbdd8e
chutki12 <priyankachaudhary2068>
2022-07-20 16:45:57 +0530

    ADDED : This Code For Welcome To Find Maximum Problem Using Generics

---
+++ MaximumGenerics/MaximumGenerics.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.maximumGenerics;
+
+public class MaximumGenerics {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome To Find Maximum Problem Using Generics");
+
+	}
+
+}
